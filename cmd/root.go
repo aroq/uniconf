d5d748a166bee644fbfffe45a693cf39d58b5913
Alexander Tolstikov <tolstikov@gmail.com>
2017-12-31 18:38:19 +0300

    Remove 'get' command as redundant

--- cmd/root.go
+++ cmd/root.go
@@ -21,6 +21,7 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/aroq/uniconf/uniconf"
 )
 
 var cfgFile string
@@ -37,7 +38,10 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+    Run: func(cmd *cobra.Command, args []string) {
+		yamlConfig := uniconf.GetYaml()
+		fmt.Println(yamlConfig)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
